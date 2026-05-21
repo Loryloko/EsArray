fd19b66f39e4e457e5a9ddeb0b832035c2c3b642
Loryloko <lorenzomem56@gmail.com>
2026-05-21 20:38:37 +0200

    Add files via upload

---
+++ Array1/script.js
@@ -0,0 +1,6 @@
+let sequenza =  [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25]
+let mostra = sequenza.sort((a, b) => b - a);
+
+console.log(mostra)
+let mostra2 = sequenza.sort((a, b) => a- b);
+console.log(mostra2)
